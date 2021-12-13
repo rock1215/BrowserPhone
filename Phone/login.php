76e5eae7e9214c3222c1576b368400663f47ec80
Yansong Wang <61701525+rock1215@users.noreply.github.com>
2021-12-13 18:06:39 +0900

    Update phone.js

--- Phone/login.php
+++ Phone/login.php
@@ -8,7 +8,7 @@ $password = 'password'; //$data->password;
 if (is_null($user) || is_null($password)) {
     echo json_encode(array('error' => 1, 'message' => 'Wrong Input!'));
 } else {
-    $outpoot = shell_exec("php ../db.php sp_login 'test;password'");
+    $outpoot = shell_exec("php ../db.php sp_login '".$user.";".$password."'");
     echo $outpoot;
 }
 
