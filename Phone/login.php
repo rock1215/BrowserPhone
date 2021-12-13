54ec4f825d4ef670e16a3caa6c123903348e45a6
Yansong Wang <61701525+rock1215@users.noreply.github.com>
2021-12-13 14:26:05 +0900

    Update login.php

--- Phone/login.php
+++ Phone/login.php
@@ -1,4 +1,13 @@
 <?php
-$outpoot = shell_exec("php ../db.php sp_login 'user;password'");
-echo $outpoot;
+
+$user = $_POST['user'];
+$password = $_POST['password'];
+
+if (is_null($user) || is_null($password)) {
+    echo json_decode('{"error": "Wrong Input"}');
+} else {
+    $outpoot = shell_exec("php ../db.php sp_login '" + $user + ";" + $password + "'");
+    echo $outpoot;
+}
+
 ?>
