d880cfb8fb76c3e4b630fe96c75746336e31a264
Yansong Wang <61701525+rock1215@users.noreply.github.com>
2021-12-13 17:34:35 +0900

    Update phone.js

--- Phone/login.php
+++ Phone/login.php
@@ -2,14 +2,16 @@
 
 $data = json_decode(file_get_contents("php://input"));
 
-$user = $data['user'];
-$password = $data['password'];
+echo json_encode($data);
 
-if (is_null($user) || is_null($password)) {
-    echo json_encode(array('error' => 1, 'message' => $data));
-} else {
-    $outpoot = shell_exec("php ../db.php sp_login '" + $user + ";" + $password + "'");
-    echo $outpoot;
-}
+// $user = $data['user'];
+// $password = $data['password'];
+
+// if (is_null($user) || is_null($password)) {
+//     echo json_encode(array('error' => 1, 'message' => $data));
+// } else {
+//     $outpoot = shell_exec("php ../db.php sp_login '" + $user + ";" + $password + "'");
+//     echo $outpoot;
+// }
 
 ?>
