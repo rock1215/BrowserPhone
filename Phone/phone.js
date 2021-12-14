9cda5c296a0a4681879012f7c6ff890b73377303
Yansong Wang <61701525+rock1215@users.noreply.github.com>
2021-12-14 16:56:27 +0900

    Update input type

--- Phone/phone.js
+++ Phone/phone.js
@@ -215,28 +215,6 @@ let lang = {};
 let audioBlobs = {};
 let newLineNumber = 0;
 
-// INPUTFILTER
-
-(function ($) {
-  $.fn.inputFilter = function (inputFilter) {
-    return this.on(
-      "input keydown keyup mousedown mouseup select contextmenu drop",
-      function () {
-        if (inputFilter(this.value)) {
-          this.oldValue = this.value;
-          this.oldSelectionStart = this.selectionStart;
-          this.oldSelectionEnd = this.selectionEnd;
-        } else if (this.hasOwnProperty("oldValue")) {
-          this.value = this.oldValue;
-          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        } else {
-          this.value = "";
-        }
-      }
-    );
-  };
-})(jQuery);
-
 // Utilities
 // =========
 function uID() {
@@ -16052,6 +16030,7 @@ function ShowLogin(loginType) {
       "<div><input id='pincode' class=UiInputText type=text maxlength=5 placeholder='PinCode' ></div>";
 
     $("#pincode").inputFilter(function (value) {
+      console.log(value);
       return /^-?\d*$/.test(value);
     });
   }
@@ -16157,4 +16136,29 @@ function ShowLogin(loginType) {
   $("#actionArea").show();
   $("#dialText").focus();
   $("#errorfield").hide();
+
+  // INPUTFILTER
+
+  (function ($) {
+    $.fn.inputFilter = function (inputFilter) {
+      return this.on(
+        "input keydown keyup mousedown mouseup select contextmenu drop",
+        function () {
+          if (inputFilter(this.value)) {
+            this.oldValue = this.value;
+            this.oldSelectionStart = this.selectionStart;
+            this.oldSelectionEnd = this.selectionEnd;
+          } else if (this.hasOwnProperty("oldValue")) {
+            this.value = this.oldValue;
+            this.setSelectionRange(
+              this.oldSelectionStart,
+              this.oldSelectionEnd
+            );
+          } else {
+            this.value = "";
+          }
+        }
+      );
+    };
+  })(jQuery);
 }
